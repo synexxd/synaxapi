56e480012de11c4b78ab8295fd8a34deecce554e
synexxd <synex2018dc@gmail.com>
2024-12-31 11:24:42 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -75,27 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   
 
-  // Update Waktu
-  function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('jam').textContent = timeString;
-  }
-  setInterval(updateTime, 1000);
-  updateTime();
-
-  // Update Tanggal
-  function updateDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    const dateString = `${day}-${month}-${year}`;
-    document.getElementById('tanggal').textContent = dateString;
-  }
+ 
   updateDate();
 });
 
