80f636b821758a1182d3f100a81876b5e1439993
synexxd <synex2018dc@gmail.com>
2025-01-12 20:43:08 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,28 +1,30 @@
-// Tema değiştirme
+// Tema Değiştirme İşlevi
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.body;
 
-// Kaydedilen tema varsa yükle
+// Kaydedilen Tema
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
   body.className = savedTheme;
   themeSwitch.checked = savedTheme === 'dark-theme';
 }
 
-// Tema değiştir
+// Tema Değişikliği
 themeSwitch.addEventListener('change', () => {
   const selectedTheme = themeSwitch.checked ? 'dark-theme' : 'light-theme';
   body.className = selectedTheme;
-
-  // Tema tercihini kaydet
   localStorage.setItem('theme', selectedTheme);
 });
 
-// Kod kopyalama işlevi
+// Kod Kopyalama
 function copyCode() {
-  const codeBlock = document.getElementById('codeBlock').innerText;
+  const codeBlock = document.querySelector('.code-block').innerText;
   navigator.clipboard.writeText(codeBlock).then(() => {
     alert('Kod başarıyla kopyalandı!');
   });
 }
 
+// Scroll İşlevi
+function scrollToSection(sectionId) {
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
